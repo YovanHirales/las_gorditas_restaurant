97c8d2939a86a386ac93291b4a3d94cb41fbee27
YovanHirales <yovanhirales@gmail.com>
2024-06-20 17:11:26 -0700

    Updated Menu June 2024

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './Menu.css';
 
-import Menu1 from '../images/Menu1.jpg';
+import Menu1 from '../images/Menu1.png';
 import Menu2 from '../images/Menu2.png';
-import Menu3 from '../images/Menu3.jpg';
-import Menu4 from '../images/Menu4.jpg';
-import Menu5 from '../images/Menu5.jpg';
-import Menu6 from '../images/Menu6.jpg';
+import Menu3 from '../images/Menu3.png';
+import Menu4 from '../images/Menu4.png';
+import Menu5 from '../images/Menu5.png';
+import Menu6 from '../images/Menu6.png';
 
 function Menu() {
 	return (
